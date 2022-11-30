3891640c6bf5c0fadc3e33f7476ebee96cdb3d57
ruta1402 <ruta.kulkarni@somaiya.edu>
2022-12-01 03:24:45 +0530

    home

--- frontend/src/Component/HomeComponent.js
+++ frontend/src/Component/HomeComponent.js
@@ -33,23 +33,26 @@ function Home() {
           <div className="container mt-4 home text-center align-self-center">
         <br/><br/>
             <div className="row mt-3 darkbg text-center justify-content-center ko">
-            <h1 style={{fontSize:"3rem",fontWeight:'600'}}> Welcome to the KJSCE Library</h1>
+            <h1 style={{fontSize:"3rem",fontWeight:'600'}}> Welcome to the Library</h1>
             </div>
             <div className="row darkbg justify-content-center">
-            <br/>
+            
             <div className="row mt-3 darkbg text-center justify-content-center koo">
-            Laura Bush in her famous quote said: “I have found the most valuable thing in my wallet is my library card.” Visit your library, get your library card, and you’ll be able to borrow a book, use free internet, or attend a course that will improve your digital skills.
+            Visit your library, get your library card, and you’ll be able to borrow a book, use free internet, or attend a course that will improve your digital skills.
             </div><br/><br/>
             <table className='tabla'>
                 <tr> <th colspan="2">Library Timings</th> </tr>
-                <tr> <td>Opening Time </td> 
-                <td> 9.00 A.M.</td></tr>
-                <tr> <td>Closing Time </td> 
-                <td> 6.00 P.M.</td></tr>
+                <tr> <td>Working Hours</td> 
+                <td> 9 am - 5 pm</td></tr>
+                <tr> <td>Working Days </td> 
+                <td> Mon - Sat</td></tr>
             </table><br/>
+                
                 </div>
+                
                 <br/><br/><br/>
             </div>
+            
         </>
         );
 }
