738c6c86c3a8aaf9e901fbc7182fcb09705220d7
ruta1402 <ruta.kulkarni@somaiya.edu>
2022-11-30 23:13:42 +0530

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 import './App.css';
 import Navbar from './Component/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Component/HomeComponent';
 import Foot from './Component/Foot';
 import AddBook from './Component/Addbook';
@@ -17,51 +17,27 @@ import { Provider } from './store/Context';
 import { initialState, reducer } from './store/userReduce';
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, initialState)
-    return ( <
-        Provider value = {
-            { state, dispatch } } >
-        <
-        Router >
-
-        <
-        Navbar / >
-        <
-        Routes >
-        <
-        Route exact path = '/'
-        element = { < Home / > }
-        /> <
-        Route path = '/Books'
-        element = { < Books / > }
-        /> <
-        Route path = '/UserDetails'
-        element = { < UserDetail / > }
-        /> <
-        Route path = '/Profile'
-        element = { < Loading / > }
-        /> <
-        Route path = '/Addbook'
-        element = { < AddBook / > }
-        /> <
-        Route path = '/History'
-        element = { < History / > }
-        /> <
-        Route path = '/Addbook'
-        element = { < Search / > }
-        /> <
-        Route path = '/Register'
-        element = { < RegistrationForm / > }
-        /> <
-        Route path = '/Login'
-        element = { < LoginForm / > }
-        /> <
-        /Routes>
-
-        <
-        /Router> <
-        /Provider>
-    );
+	const [state,dispatch] = useReducer(reducer,initialState)
+return (
+	<Provider value={{state,dispatch}}>
+	<Router>
+	
+	<Navbar />
+	<Routes>
+		<Route exact path='/' element={<Home />} />
+		<Route path='/Books' element={<Books />} />
+		<Route path='/UserDetails' element={<UserDetail />} />
+		<Route path='/Profile' element={<Loading />} />
+		<Route path='/Addbook' element={<AddBook />} />
+		<Route path='/History' element={<History />} />
+		<Route path='/Addbook' element={<Search />} />
+		<Route path='/Register' element={<RegistrationForm />} />
+		<Route path='/Login' element={<LoginForm />} />
+	</Routes>
+	
+	</Router>
+	</Provider>
+);
 }
 
 export default App;
